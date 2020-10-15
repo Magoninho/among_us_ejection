1b1042bb9cf40156996ca16c15f0b6ff0cee12b8
magoninho <magoninhogamer@gmail.com>
2020-10-15 20:19:37 -0300

    gui

--- astronauts.py
+++ astronauts.py
@@ -1,29 +1,26 @@
+# -- coding: UTF-8 --
 import pygame, sys
 from SimpleDrawEngine import *
 
-pos_x = -360
-vx = 10
+
 class Player:
-    def __init__(self, screen, imagem, rect, angle):
+    def __init__(self, screen, imagem, rect, angle, pos_x):
         
         self.screen = screen
         self.imagem = pygame.image.load(imagem).convert()
         self.rect = rect
         self.angle = angle
+        self.pos_x = pos_x
 
     def animation(self):
-        global pos_x, vx
         ## scaling the image
         self.imagem = pygame.transform.scale(self.imagem, (150, 150))
         ## 
         self.rect = self.imagem.get_rect()
-        self.rect.center = (pos_x, 300)
+        self.rect.center = (self.pos_x, 300)
         old_center = self.rect.center
         new_image = self.imagem = pygame.transform.rotate(self.imagem, self.angle)
         self.rect = new_image.get_rect()
         self.rect.center = old_center
-        pos_x += vx
-        vx -= 0.035
-        if vx <= 0:
-            vx = 0
+        
         self.screen.blit(self.imagem, self.rect)
--- main.py
+++ main.py
@@ -2,71 +2,77 @@ import sys, random, pygame, os
 from SimpleDrawEngine import *
 from astronauts import *
 
+
 ## Limpador de tela multiplataforma Magoninho Gamer versão 1.2
 def limpa_tela():
     os.system('cls' if os.name=='nt' else 'clear')
 
 limpa_tela()
 
-print("\u001b[0mSelecione o personagem escrevendo algum deles dessa lista:\n\u001b[31m-red\n\u001b[33m-yellow\n\u001b[32m-green\n\u001b[36m-cyan\u001b[0m")
-file = input("Personagem: ")
 
-pygame.init()
+def main(color):
+	pygame.init()
 
-N = 200
+	N = 200
+	white = (255, 255, 255)
+	black = (0,0,0)
+	red = (255, 0, 0)
+	largura, altura = 800, 600
 
-white = (255, 255, 255)
-black = (0,0,0)
-red = (255, 0, 0)
-largura, altura = 800, 600
+	screen = pygame.display.set_mode((largura, altura))
+	caption = pygame.display.set_caption("ALPHA")
 
-screen = pygame.display.set_mode((largura, altura))
-caption = pygame.display.set_caption("ALPHA")
-
-# nao funcionou
-# for star in range(N):	
-# 	x, y = random.randint(0, largura), random.randint(0, altura)
-# 	Rect(screen, (255, 255, 255), (x, y, 1, 1))
+	# nao funcionou
+	# for star in range(N):	
+	# 	x, y = random.randint(0, largura), random.randint(0, altura)
+	# 	Rect(screen, (255, 255, 255), (x, y, 1, 1))
 
 
-stars = [
-	[random.randint(0, largura), random.randint(0, altura)] for x in range(N)
-]
+	stars = [
+		[random.randint(0, largura), random.randint(0, altura)] for x in range(N)
+	]
 
 
-pygame.mixer.init()
-pygame.mixer.music.load('sound.mp3')
-pygame.mixer.music.play(-1)
+	pygame.mixer.init()
+	pygame.mixer.music.load('sound.mp3')
+	pygame.mixer.music.play(-1)
 
-clock = pygame.time.Clock()
+	clock = pygame.time.Clock()
+	file = color
+	## objetos
+	test = 45
+	pos_x = -360
+	vx = 10
+	angle = 0
+	while True:	
+		clock.tick(30)
+		player = Player(screen, ("characters/" + file + ".png"), (0, 0), angle, pos_x)
+		angle += 5
+		
+		
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				
+		screen.fill(black)
 
-## objetos
-test = 45
 
-angle = 0
-while True:	
-	clock.tick(30)
-	player = Player(screen, ("characters/" + file + ".png"), (0, 0), angle)
-	angle += 5
-	
-	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-	screen.fill(black)
-
-
-	# Drawinings
-	## Stars
-	for star in stars:
-		Rect(screen, white, (star[0], star[1], 2, 2))
-		star[0] -= 0.5
-		if star[0] < 0:
-			star[0] = 800
-	## Astronauts
-	# screen.blit(astronaut1, astronaut1_rect)
-	player.animation()
-	# pygame.draw.rect(screen, white, (325, 225, 174, 174))
-	## Update
-	pygame.display.flip()
+		# Drawinings
+		## Stars
+		for star in stars:
+			Rect(screen, white, (star[0], star[1], 2, 2))
+			star[0] -= 0.5
+			if star[0] < 0:
+				star[0] = 800
+		## Astronauts
+		player.animation()
+		
+		pos_x += vx
+		vx -= 0.035
+		print(vx)
+		if vx <= 0:
+			vx = 0
+		
+		## Update
+		pygame.display.flip()
